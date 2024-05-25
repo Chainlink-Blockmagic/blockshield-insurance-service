9501087b4275cd6671c674a7c9e4594486c4bbb1
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2024-05-25 20:51:54 -0300

    fix: update the find by wallet query named function to ignore case

--- src/main/kotlin/com/blockshield/insurance/service/resources/transaction/TransactionRepository.kt
+++ src/main/kotlin/com/blockshield/insurance/service/resources/transaction/TransactionRepository.kt
@@ -4,9 +4,11 @@ import com.blockshield.insurance.service.domain.transaction.entity.Transaction
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 import java.util.UUID
 
 interface TransactionRepository : MongoRepository<Transaction, UUID> {
 
+    @Query("{ 'wallet' : {\$regex : ?0, \$options: 'i'}}")
     fun findByWallet(wallet: String, pageable: Pageable): Page<Transaction>
 }
