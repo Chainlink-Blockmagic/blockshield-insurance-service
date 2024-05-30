58d9a0a2aa10d64fa3c503216991a1f5ce148ee6
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2024-05-30 16:55:18 -0300

    feat: update to add function to find asset by due date and active true

--- src/main/kotlin/com/blockshield/insurance/service/resources/asset/AssetRepository.kt
+++ src/main/kotlin/com/blockshield/insurance/service/resources/asset/AssetRepository.kt
@@ -4,8 +4,12 @@ import com.blockshield.insurance.service.domain.asset.entity.Asset
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.Query
+import java.time.LocalDate
 import java.util.*
 
+/**
+ * https://docs.spring.io/spring-data/mongodb/reference/mongodb/repositories/query-methods.html
+ */
 interface AssetRepository : MongoRepository<Asset, UUID> {
 
     @Query("{ 'insuranceTokenAddress' : {\$regex : ?0, \$options: 'i'}}")
@@ -14,4 +18,6 @@ interface AssetRepository : MongoRepository<Asset, UUID> {
     fun findBySymbol(symbol: String): Optional<Asset>
 
     fun findByActiveIsTrue(pageable: Pageable): List<Asset>
+
+    fun findByDueDateLessThanAndActiveIsTrue(dueDate: LocalDate): List<Asset>
 }
